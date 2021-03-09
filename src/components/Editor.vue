7ad6867101ec97c26b198d8683a73da8441996c7
yuki-snow1823 <magic.iroha@gmail.com>
2021-03-10 08:32:38 +0900

    init

---
+++ src/components/Editor.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- Two-way Data-Binding -->
+  <quill-editor
+    ref="myQuillEditor"
+  />
+
+</template>
+
+<script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
+
+export default {
+  components: {
+    quillEditor
+  },
+  name: 'VueQuillEditor',
+
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+</style>
