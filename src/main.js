49269a7c35eb55970a332a3de8e1327cf67dff50
yuki-snow1823 <magic.iroha@gmail.com>
2021-03-11 06:12:25 +0900

    delete: markdown

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueQuillEditor from "vue-quill-editor";
+
 import "quill/dist/quill.core.css";
 import "quill/dist/quill.snow.css";
 import "quill/dist/quill.bubble.css";
