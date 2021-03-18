8eebec60e66dcf6b8cc7021fdcad664377937be3
yuki-snow1823 <magic.iroha@gmail.com>
2021-03-19 08:25:20 +0900

    add: computedの学習

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,14 +1,44 @@
 <template>
-  <div id="preview" class="content ql-editor" v-html="textData"></div>
+  <div class="viewer__wrapper">
+    <div class="first-text">{{ firstTextData }}</div>
+    <div id="preview" class="content ql-editor" v-html="textData"></div>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+    }
+  },
   props: {
     textData: {
      type: String,
      default: ''
     }
+  },
+  computed: {
+    // ここ発表会で相談したい
+    firstTextData: function () {
+      let text = "ここにテキストが入ります"
+      
+      if(this.textData) {
+        return ""
+      }
+      
+      return text
+    }
   }
 }
 </script>
+
+<style>
+/* .first-text {
+  margin-top: 24px;
+} */
+.viewer__wrapper {
+  margin-top: 30px;
+  width: 70%;
+  border: 2px rgb(105, 105, 105) dotted;
+}
+</style>
